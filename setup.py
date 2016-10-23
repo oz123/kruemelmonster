b5a71d19f5eb75590f7fd2535493b11ad9c2911b
Oz N Tiram <oz.tiram@gmail.com>
2016-10-23 15:20:29 -0400

    fix typos in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name='krunmelmonster',
+    name='krumelmonster',
     version='0.0.2',
     author='Oz Tiram',
     author_email='oz.tiram@gmail.com',
@@ -10,7 +10,7 @@ setup(
     packages=find_packages(exclude=['tests']),
     license='LGPL',
     description='WSGI sessions implentation (session id in a cookie).',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     platforms='any',
     download_url='http://pypi.python.org/pypi/krumelmonster/',
 )
