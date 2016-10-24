edcdf8e0801295ad1f9587fabcbc83c3d5b6c064
Oz N Tiram <oz.tiram@gmail.com>
2016-10-24 14:46:07 -0400

    Practice TDD

--- tests/test_peewee_session.py
+++ tests/test_peewee_session.py
@@ -13,4 +13,19 @@ session_manager = SqliteSessionManager(db, PeeweeSession,
                                        ttl=60, ttl_unit='seconds')
 
 
-assert os.path.exists('sessions.db')
+
+def test_session_db_created():
+    assert os.path.exists('sessions.db')
+
+
+def test_session_db_is_not_locked():
+    assert False
+
+
+def test_sessions_inserted():
+    assert False
+
+
+def test_trigger_works():
+    # add trigger
+    assert False
