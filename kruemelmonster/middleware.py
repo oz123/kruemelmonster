31fb02e11345ebdb44590387176e4b5eef8d1fe1
Oz N Tiram <oz.tiram@gmail.com>
2017-01-12 23:06:22 +0100

    Remove the Session class with a factory

--- kruemelmonster/middleware.py
+++ kruemelmonster/middleware.py
@@ -38,32 +38,3 @@ class SimpleSessionMiddleware:
             response_headers.append(('Set-Cookie', cookie_string))
             return start_response(status, response_headers, exc_info)
         return self.app(environ, middleware_start_response)
-
-
-class SessionMiddleware:
-
-    def __init__(self, app, factory,
-                 env_key='wsgisession', cookie_key='session_id'):
-        self.app = app
-        self.factory = factory
-        self.env_key = env_key
-        self.cookie_key = cookie_key
-
-    def __call__(self, environ, start_response):
-        cookie = SimpleCookie()
-        if 'HTTP_COOKIE' in environ:
-            cookie.load(environ['HTTP_COOKIE'])
-        id = None
-        if self.cookie_key in cookie:
-            id = cookie[self.cookie_key].value
-        environ[self.env_key] = self.factory.load(id)
-
-        def middleware_start_response(status, response_headers, exc_info=None):
-            id = self.factory.save(environ[self.env_key])
-            cookie = SimpleCookie()
-            cookie[self.cookie_key] = id
-            cookie[self.cookie_key]['path'] = '/'
-            cookie_string = cookie[self.cookie_key].OutputString()
-            response_headers.append(('Set-Cookie', cookie_string))
-            return start_response(status, response_headers, exc_info)
-        return self.app(environ, middleware_start_response)
